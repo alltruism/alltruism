b2dee5f27d6bebfb3b51595032fc70f6fd81c69f
Amy Lai <amyjlai@hotmail.com>
2015-01-04 16:26:49 -0500

    Add validations to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ActiveRecord::Base
+  belongs_to :organization
   has_many :event_volunteers
   has_many :users, :through => :event_volunteers
   attr_accessible :description, :end_time, :location, :name, :organization_id, :start_time
+  validates_presence_of :name, :description, :location, :start_time
 end
