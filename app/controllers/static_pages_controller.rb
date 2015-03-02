b7deeff0e5f288bafcb88f60e5c9ffaab484cba5
Amy Lai <amyjlai@hotmail.com>
2015-03-01 21:04:59 -0500

    Remove most content from homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,9 +11,6 @@ class StaticPagesController < ApplicationController
     if o = Organization.where('upper(name) like ?', "%#{search.upcase}%")
       @matches += o
     end
-    if u = User.where('upper(fname) like ? OR upper(lname) like ?', search.upcase, search.upcase)
-      @matches += u
-    end
     if e = Event.where('upper(name) like ?', "%#{search.upcase}%")
       @matches += e
     end
